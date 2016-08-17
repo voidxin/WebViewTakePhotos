c05cb6910068b4e8d362e5f27c3d379cc077b595
voidxin <zhangxin409999@163.com>
2016-08-17 13:16:25 +0800

    first commit

--- takePhotosDemo/ViewController.m
+++ takePhotosDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic,strong)UIWebView *webView;
 @end
 
 @implementation ViewController
@@ -17,11 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.webView.frame=CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self.view addSubview:self.webView];
+    [self loadLocalHTMLFile];
 }
-
+-(void)loadLocalHTMLFile{
+    NSString *path=[[NSBundle mainBundle] pathForResource:@"camera" ofType:@"html"];
+    NSURL *url=[NSURL fileURLWithPath:path];
+    NSURLRequest *request=[NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIWebView *)webView{
+    if (!_webView) {
+        _webView=[[UIWebView alloc]init];
+    }
+    return _webView;
 }
 
 @end
